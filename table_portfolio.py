fe333447f31b26f4d6f3b03a087799e2b70d5654
AdoNunes <adonay.s.nunes@gmail.com>
2021-04-05 22:02:36 -0400

    ENH table search

--- table_portfolio.py
+++ table_portfolio.py
@@ -26,9 +26,12 @@ def get_raw_data():
     cols = [c for c in data.columns if c not in cols_out]
     
     data = data[cols]
-    
-    data.loc[14, 'Date'] = data.loc[14, 'Date'] + '.654773'
-    data.loc[15, 'Date'] = data.loc[15, 'Date'] + '.654773'
+    try:
+        data.loc[14, 'Date'] = data.loc[14, 'Date'] + '.654773'
+        data.loc[15, 'Date'] = data.loc[15, 'Date'] + '.654773'
+    except:
+        pass
+            
     
     data['Date'] = data['Date'].apply(lambda x: short_date(x))
     
@@ -52,10 +55,29 @@ def get_raw_data():
 
 
 hists = ['option_alerts_message_history.csv']
-def get_hist_msgs():
-    data = pd.read_csv("data/option_alerts_message_history.csv")
+
+def get_hist_msgs(filt_author='', filt_date_frm='', filt_date_to='',
+                  filt_cont='', **kwargs):
+    # Provide arguments to filter
+    
+    data = pd.read_csv("data/option_alerts_message_history_copy.csv")
     cols = ['Author', 'Date', 'Content']
     data = data[cols]
+    
+    data = data[~data['Author'].str.contains('Xcapture')]
+    data['Author'] = data['Author'].apply(lambda x: x.split('#')[0])
+    data['Date'] = data['Date'].apply(lambda x: short_date(x))
+    
+    data = data.dropna()
+    if filt_author:
+        data = data[data['Author'].str.contains(filt_author, case=False)]
+    if filt_date_frm:
+        data = data[data['Date'] > filt_date_frm]
+    if filt_date_to:
+        data = data[data['Date'] < filt_date_to]
+    if filt_cont:
+        data = data[data['Content'].str.contains(filt_cont, case=False)]
+
     header_list = data.columns.tolist()
     return data.values.tolist(), header_list
 
@@ -63,31 +85,48 @@ def get_hist_msgs():
 data, header_list = get_raw_data() 
 
 
+
+
 layout_table = [
+    [sg.Button("Update", button_color=('white', 'black'), key="-UPDATE-")],
     [sg.Table(values=data,
                headings=header_list,
               display_row_numbers=True, vertical_scroll_only=False,
               auto_size_columns=True,
+              font=("courier", 19),
               # alternating_row_color='lightblue',
               num_rows=len(data), key='_TABLE_')],
-    [sg.Button("Update", button_color=('white', 'black'), key="-UPDATE-")]
 ]
 
 data, header_list = get_hist_msgs()
+data[0][2] = 'This is my output string\nThat spans multiple rows\nAll the way to 3\n'+ \
+                                   'It will even wrap if the string is too long'
+
 layout_msg = [
+    [sg.Text('Filter: Author '), sg.Input(key='filt_author', size=(20, 1)),
+     sg.Text(' '*10),
+     sg.Text('Date from: '), sg.Input(key='filt_date_frm', size=(10, 1)),
+     sg.Text(' To: '), sg.Input(key='filt_date_to', size=(10, 1)),
+     sg.Text(' '*10),
+     sg.Text('Message contains: '), sg.Input(key='filt_cont', size=(20, 1)),
+     ],
+    [sg.ReadFormButton("Update", button_color=('white', 'black'), key="-UPDATE2-")],
     [sg.Table(values=data,
                headings=header_list, justification='left',
               display_row_numbers=False,
+              max_col_width=200, text_color='black',
+              font=("courier", 19),
               # auto_size_columns=True, 
               vertical_scroll_only=False,
-              # alternating_row_color='lightblue',
-              col_widths=[30,30, 1300],
-              num_rows=10, key='_HIST_')],
-    [sg.Button("Update", button_color=('white', 'black'), key="-UPDATE2-")]
+               alternating_row_color='grey',
+              # col_widths=[30,300, 1300],
+              row_height=25,
+              num_rows=30, key='_HIST_')],
+    
 ]
 
 
-layout = [[sg.TabGroup([[sg.Tab('Portfolio', layout_table), sg.Tab('Tab 2', layout_msg)]])],
+layout = [[sg.TabGroup([[sg.Tab('Portfolio', layout_table), sg.Tab('MSGs', layout_msg, k="kk")]])],
               ]
 
 window = sg.Window('Xtrader', layout, 
@@ -107,8 +146,20 @@ while True:
         window.Element('_TABLE_').Update(values=dt, num_rows=len(data))
 
     elif event == "-UPDATE2-":
-        print("Updateing!")
-        window.Element('_HIST_').expand(expand_x=True)
+        print("Updating!")
+        print(values)
+        
+        window['_HIST_'].AutoSizeText=True
+        del values[0]
+        dt, _  =  get_hist_msgs(**values)
+        window.Element('_HIST_').Update(values=dt, num_rows=len(data))
+        
 
+# wraplen = window['_HIST_'].Widget.winfo_reqwidth()  # width tkinter says widget will be
+#             window['-OUT-'].Widget.configure(wraplen=wraplen) 
+            
 window.close()
 
+
+# borderwidth
+# colormap
