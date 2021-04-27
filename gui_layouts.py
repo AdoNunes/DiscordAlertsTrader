7387d4db1529d4dd278b85ae9e4a016568373796
AdoNunes <adonay.s.nunes@gmail.com>
2021-04-27 15:32:59 -0400

    GUI QT hist chans

---
+++ gui_layouts.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 27 11:54:36 2021
+
+@author: adonay
+"""
+
+import PySimpleGUIQt as sg
+import pandas as pd
+from datetime import datetime
+from gui_generator import (get_portf_data, get_hist_msgs)
+from place_order import get_TDsession
+import gui_generator as gg
+from PySide2.QtGui import QPainter, QPixmap, QPen, QColor
+from PySide2.QtWidgets import QHeaderView
+
+
+def layout_portfolio(data_n_headers, font_body, font_header):
+    layout = [
+         [sg.Column([[sg.Button("Update", button_color=('white', 'black'), key="_upd-portfolio_")]])],
+         [sg.Table(values=data_n_headers[0],
+                        headings=data_n_headers[1],
+                        display_row_numbers=True,
+                        auto_size_columns=True,
+                        header_font=font_header,
+                        text_color='black',
+                        font=font_body,
+                        justification='left',
+                        alternating_row_color='grey',
+                        # num_rows=30, #len(data_n_headers[0]),
+                        key='_portfolio_')]
+         ]
+    return layout
+
+
+def layout_chan_msg(chn, data_n_headers, font_body, font_header):
+    layout = [
+        [sg.Text('Filter:  Author: ', size=(20, 1)), sg.Input(key=f'{chn}_filt_author', size=(20, 1)),
+           # sg.Text(' '*2),
+         sg.Text('Date from: ', size=(15, 1)), sg.Input(key=f'{chn}_filt_date_frm', size=(10, 1), default_text='02/09'),
+         sg.Text(' To: ', size=(5, 1)), sg.Input(key=f'{chn}_filt_date_to', size=(10, 1)),
+          # sg.Text(' '*1),
+         sg.Text('Message contains: ', size=(25, 1)), sg.Input(key=f'{chn}_filt_cont', size=(20, 1)),
+         sg.Text('Num. rows display: '), sg.Input(key=f'{chn}_n_rows', size=(5, 1)),
+         ],
+        [sg.ReadFormButton("Update", button_color=('white', 'black'), key=f'{chn}_UPD', bind_return_key=True)],
+        [sg.Table(values=data_n_headers[0],
+                  headings=data_n_headers[1],
+                  justification='left',
+                  display_row_numbers=False,
+                  max_col_width=2000, text_color='black',
+                  font=font_body,
+                  col_widths=[30,300, 1300],
+                  header_font=font_header,
+                  # auto_size_columns=True,
+                  # vertical_scroll_only=False,
+                   alternating_row_color='grey',
+                  # col_widths=[30,300, 1300],
+                  # row_height=20,
+                  num_rows=30, 
+                  key=f"{chn}_table")]
+        ]
+    return layout
+
+
+def tt_acnt(text, fsize=12, bold=True, underline=True, font_name="Arial", size=None):
+    font = [font_name, fsize]
+    if bold: font += ['bold']
+    if underline: font += ["underline"]
+    if size is None:
+        size = (len(text) *2, 1)
+    return sg.T(text,font=font,size=size)
+
+
+def row_cols(cols, vals=("Grey", "Brown")):
+    # cols = bool x n rows
+    return list(map(lambda a: (a[0], vals[0]) if a[1] else (a[0], vals[1]), enumerate(cols)))
+
+
+def layout_account(TDSession, font_body, font_header):
+    
+    acc_inf, ainf = gg.get_acc_bals(TDSession)
+    pos_tab, pos_headings = gg.get_pos(acc_inf)
+    ord_tab, ord_headings, cols= gg.get_orders(acc_inf)
+    
+    layout = [[sg.Column([
+        [tt_acnt("Account ID:"), tt_acnt(ainf["id"], font_body[1], 0, 0, font_body[0]),
+         tt_acnt("Balance:"), tt_acnt("$" + str(ainf["balance"]), font_body[1], 0, 0, font_body[0]),
+         tt_acnt("Cash:"), tt_acnt("$" + str(ainf["cash"]), font_body[1], 0, 0, font_body[0]),
+         tt_acnt("Funds:"), tt_acnt("$" + str(ainf["funds"]), font_body[1], 0, 0, font_body[0])
+         ]])],
+        [sg.Column(
+            [
+             [sg.T("Positions", font=(font_header[0], font_header[1], 'bold', "underline"),size=(20,1.5))],
+             [sg.Table(values=pos_tab, headings=pos_headings,justification='left',
+              display_row_numbers=False, text_color='black', font=font_body,
+               auto_size_columns=True,header_font=font_header,    
+              alternating_row_color='grey',
+              # col_widths=[30,300, 1300],
+              # row_height=20,
+              key='_positions_')]]),
+        sg.Column(
+            [
+             [sg.T("Orders",font=(font_header[0], font_header[1], 'bold',              "underline"),size=(20,1.5))],
+             [sg.Table(values=ord_tab, headings=ord_headings,justification='left',
+              display_row_numbers=False, text_color='black', font=font_body,
+              auto_size_columns=True, header_font=font_header,
+              key='_orders_')]])
+            ]]
+    return layout
+
+
+
+
+# window['_HIST_'].set_vscroll_position(1)
+
+# window['_orders_'].Widget.config(width=8)
+
+# sg.Print('This text is white on a green background', text_color='white', background_color='green', font='Courier 10')
+# sg.Print('The first call sets some window settings like font that cannot be changed')
+
+
+#sg.popup_scrolled('your_table = [ ', ',\n'.join([str(table[i]) for i in range(MAX_ROWS)]) + '  ]', title='Copy your data from here', font='fixedsys', keep_on_top=True)
+
+
+
+
+
+
+
+
+
+
+
---
+++ table_portfolio_qt.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  3 18:18:43 2021
+
+@author: adonay
+"""
+import PySimpleGUIQt as sg
+import pandas as pd
+from datetime import datetime
+from place_order import get_TDsession
+import gui_generator as gg
+import gui_layouts as gl
+from PySide2.QtGui import QPainter, QPixmap, QPen, QColor
+from PySide2.QtWidgets import QHeaderView
+
+TDSession = get_TDsession()
+
+# sg.SetOptions(font=("Courier New", -13))#, background_color="whitesmoke",
+               # element_padding=(0, 0), margins=(1, 1))
+
+
+fnt_b = ("Helvitica", "11")
+fnt_h = ("Helvitica", "10")
+
+gui_data = {}
+gui_data['port'] = gg.get_portf_data()
+ly_port = gl.layout_portfolio(gui_data['port'], fnt_b, fnt_h)
+
+chns = ["stock_alerts", "option_alerts"]
+# chns = ["option_alerts"]#, ""]
+ly_chns = []
+for chn in chns:
+    gui_data[chn] = gg.get_hist_msgs(chan_name=chn)
+    ly_ch = gl.layout_chan_msg(chn, gui_data[chn], fnt_b, fnt_h)
+    ly_chns.append(ly_ch)
+
+ly_accnt = gl.layout_account(TDSession, fnt_b, fnt_h)
+
+
+layout = [[sg.Column([[sg.TabGroup([[sg.Tab('Portfolio', ly_port)],
+                                    [sg.Tab(c, h) for c, h in zip(chns, ly_chns)],
+                                    [sg.Tab("Account", ly_accnt)]])]])]]
+           
+
+window = sg.Window('Xtrader', layout,# force_toplevel=True,
+                   size=(3000, 50), auto_size_text=False, resizable=True, finalize=True)
+
+
+# window['_HIST_'].set_vscroll_position(1)
+
+# window['_orders_'].Widget.config(width=8)
+
+# sg.Print('This text is white on a green background', text_color='white', background_color='green', font='Courier 10')
+# sg.Print('The first call sets some window settings like font that cannot be changed')
+
+
+#sg.popup_scrolled('your_table = [ ', ',\n'.join([str(table[i]) for i in range(MAX_ROWS)]) + '  ]', title='Copy your data from here', font='fixedsys', keep_on_top=True)
+
+
+def fit_table_elms(Widget_element):
+    Widget_element.resizeRowsToContents()
+    Widget_element.resizeColumnsToContents()
+    Widget_element.resizeRowsToContents()
+    Widget_element.resizeColumnsToContents()
+
+# event, values = window.read(.1)
+
+els = ['_portfolio_', '_orders_', '_positions_'] + [f"{chn}_table" for chn in chns]
+for el in els:
+    fit_table_elms(window.Element(el).Widget)
+
+for chn in chns:
+    table = window[f"{chn}_table"].Widget.horizontalHeader()
+    table.setSectionResizeMode(2, QHeaderView.Stretch)
+    # table.setSectionResizeMode(2, QHeaderView.ResizeToContents)
+    window[f"{chn}_table"].Widget.scrollToBottom()
+
+# i = window.Element('_PORT_').Widget.item(1,1)
+# i.setBackground(QColor('red'))
+event, values = window.read(.5)
+while True:
+    event, values = window.read()
+    print(event)
+    if event == sg.WINDOW_CLOSED:
+        break
+
+    elif event == "_upd-portfolio_":
+        print("Updateing!")
+        dt, hdr = gg.get_portf_data()
+        # window.Element('_portfolio_').expand(expand_x=True)
+        window.Element('_portfolio_').Update(values=dt, num_rows=len(dt))
+
+    elif event[-3:] == "UPD":
+        chn = event[:-4]
+        print(f"Updating {chn}!", values)
+        # window[f"{chn}_table"].AutoSizeText=True
+
+        args = {}
+        for k, v in values.items():
+            if k[:len(chn)] == chn:
+                args[k[len(chn)+1:]] = v
+        dt, _  = gg.get_hist_msgs(chan_name=chn, **args)
+        if args['n_rows'] != "":
+            n_rows = eval(args['n_rows'])
+            n_rows = max(1, n_rows)
+            window.Element(f"{chn}_table").Update(values=dt,  num_rows=n_rows)
+        else:
+            window.Element(f"{chn}_table").Update(values=dt)
+            
+        fit_table_elms(window.Element(f"{chn}_table").Widget)
+
+window.close()
+
+
