bb213f853e5205ac8d7a441409786417ac0abb86
adonunes <adonay.s.nunes@gmail.com>
2023-05-25 06:40:44 -0400

    with discord

--- DiscordAlertsTrader/gui.py
+++ DiscordAlertsTrader/gui.py
@@ -280,7 +280,7 @@ def gui():
     client_thread = threading.Thread(target=run_client)
 
     # start the threads
-    # client_thread.start()
+    client_thread.start()
     run_gui()
 
     # close the GUI window
