6a385b618ee33bae1cf88860913293b93ac1b506
adonunes <adonay.s.nunes@gmail.com>
2023-06-28 21:30:52 -0400

    test sto

--- tests/test_AlertsTrader.py
+++ tests/test_AlertsTrader.py
@@ -31,7 +31,7 @@ class TestAlertsTrader(unittest.TestCase):
         
         cfg['order_configs']['max_trade_capital'] = '1000'
         cfg['discord']['notify_alerts_to_discord'] = 'false'
-        
+
         trader = AlertsTrader(brokerage, 
                               portfolio_fname=self.trader_portfolio_fname,
                               alerts_log_fname=self.trader_log_fname,
@@ -39,6 +39,7 @@ class TestAlertsTrader(unittest.TestCase):
                               queue_prints=queue.Queue(maxsize=50),
                               cfg=cfg
                               )
+        
         
         # Expected values
         expected = {
@@ -59,6 +60,7 @@ class TestAlertsTrader(unittest.TestCase):
             "STC1-ordID": 100,
             
             }
+
         expected["PnL"] =  100*(expected["STC1-Price"]- expected["Price"])/ expected["Price"]
         expected["$PnL"] =  round(expected["PnL"] * expected["STC1-uQty"] * expected["Price"],1)
         expected["PnL-Alert" ] = 100*(expected["STC1-Price-Alerted"]- expected["Price-Alert"])/ expected["Price-Alert"]
@@ -132,6 +134,129 @@ class TestAlertsTrader(unittest.TestCase):
             else:
                 self.assertEqual(trade[exp], val)
 
+    def tracker_order_sto(self, brokerage):
+        """Test that the TDA order is sent correctly and trader works as expected for selling to open"""
+        self.trader_portfolio_fname=root_dir+"/data/test_trader_portfolio.csv"
+        self.trader_log_fname=root_dir+"/data/test_trader_log.csv"
+        # remove in case they exist
+        if os.path.exists(self.trader_portfolio_fname):
+            os.remove(self.trader_portfolio_fname)
+        if os.path.exists(self.trader_log_fname):
+            os.remove(self.trader_log_fname)
+        
+        cfg['order_configs']['max_trade_capital'] = '1000'
+        cfg['discord']['notify_alerts_to_discord'] = 'false'
+        cfg['shorting']['DO_STO_TRADES'] = 'true'
+        cfg['shorting']['DO_BTC_TRADES'] = 'true'
+        cfg['shorting']['BTC_SL'] = '50'
+        cfg['shorting']['BTC_PT'] = '50'
+        
+        trader = AlertsTrader(brokerage, 
+                              portfolio_fname=self.trader_portfolio_fname,
+                              alerts_log_fname=self.trader_log_fname,
+                              update_portfolio=False,
+                              queue_prints=queue.Queue(maxsize=50),
+                              cfg=cfg
+                              )
+        
+        # Expected values
+        expected = {
+            'BTO-Status': 'FILLED',
+            "Type": "STO",
+            'Price': 2,
+            "ordID": 99,
+            'Asset': 'option',
+            'Price-Alert': 2.1,
+            'Price-Current': 2.2,
+            'uQty': 5,
+            'filledQty': 5,
+            'exit_plan': "{'PT1': None, 'PT2': None, 'PT3': None, 'SL': None}",
+            "STC1-uQty": 5,
+            "STC1-Price": 1.05,
+            "STC1-Price-Current": 1.1,
+            "STC1-Price-Alerted": 1.5,
+            "STC1-Status": "FILLED",
+            "STC1-ordID": 100,
+            
+            }
+        exit_plan = eval(expected['exit_plan'])
+        exit_plan['PT1'] = round(expected['Price'] * (1 - float(cfg['shorting']['BTC_PT'])/100),2)
+        exit_plan['SL'] = round(expected['Price'] * (1 + float(cfg['shorting']['BTC_SL'])/100),2)
+        expected['exit_plan'] = str(exit_plan)  
+        
+        expected["PnL"] =  100*(expected["Price"] - expected["STC1-Price"])/ expected["Price"]
+        expected["$PnL"] =  round(expected["PnL"] * expected["STC1-uQty"] * expected["Price"],1)
+        expected["PnL-Alert" ] = 100*(expected["Price-Alert"] - expected["STC1-Price-Alerted"])/ expected["Price-Alert"]
+        expected["$PnL-Alert"] =  expected["PnL-Alert"] * expected["STC1-uQty"] * expected["Price-Alert"]
+        expected["PnL-Current"] =  100*(expected["Price-Current"] - expected["STC1-Price-Current"])/ expected["Price-Current"]
+        expected["$PnL-Current"] =  expected["PnL-Current"] * expected["STC1-uQty"] * expected["Price-Current"]
+
+        # Create a message, order and pars
+        message = make_message()
+        expdate = datetime.now().strftime("%m/%d")
+        message.content = f'STO {expected["uQty"]} AI 25c {expdate} @ {expected["Price-Alert"]}'
+        pars, order =  parse_trade_alert(message.content)
+        order['Trader'] = f"{message.author.name}#{message.author.discriminator}"
+        order["Date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        
+        # Generate return vals for the brokerage
+        symbol = f'AI_{expdate.replace("/", "")}23C25'
+        brokerage.get_quotes.return_value = {symbol: {'bidPrice': expected['Price-Current']}}
+        brokerage.send_order.return_value = [
+            expected['BTO-Status'],
+            expected["ordID"]         
+            ]
+        brokerage.get_order_info.return_value = [
+            expected['BTO-Status'],
+            {'quantity': expected["uQty"],
+             "price": expected['Price'],
+             'filledQuantity': expected["uQty"],
+             'status': expected['BTO-Status'],
+             'orderLegCollection':[
+                {'instrument': {'symbol': symbol},
+                 'instruction': "SELL_TO_OPEN"},  
+            ]}]
+        
+        trader.new_trade_alert(order, pars, message.content)
+        
+        # make STC order
+        message.content = f'BTC {expected["STC1-uQty"]} AI 25c {expdate} @ {expected["STC1-Price-Alerted"]}'
+        pars, order =  parse_trade_alert(message.content)
+        order['Trader'] = f"{message.author.name}#{message.author.discriminator}"
+        order["Date"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+        
+        # Generate return vals for the brokerage
+        brokerage.get_quotes.return_value = {f'AI_{expdate.replace("/", "")}23C25': {'askPrice': expected["STC1-Price-Current"]}}
+        brokerage.send_order.return_value = [
+            expected['STC1-Status'],
+            expected["STC1-ordID"]         
+            ]
+        brokerage.get_order_info.return_value = [
+            expected['STC1-Status'],
+            {'status': expected['STC1-Status'],
+             'quantity': expected["STC1-uQty"],
+             'filledQuantity': expected["STC1-uQty"],
+             "price": expected['STC1-Price'],
+             'orderLegCollection':[{
+                 'instrument': {'symbol': symbol},
+                 'instruction': "BUY_TO_CLOSE", 
+                 "quantity": expected["STC1-uQty"]}],
+             'closeTime' : order["Date"]
+             }         
+            ]
+        
+        trader.new_trade_alert(order, pars, message.content)
+        
+        # assert expected values
+        trade = trader.portfolio.loc[0]
+        for exp, val in expected.items():
+            if not trade[exp] == val:
+                print(f"Expected {exp} = {val} but got {trade[exp]}")
+            if isinstance(trade[exp], float):
+                self.assertAlmostEqual(trade[exp], val, places=2)
+            else:
+                self.assertEqual(trade[exp], val)
+    
         # Delete the generated file
         os.remove(self.trader_portfolio_fname)
         os.remove(self.trader_log_fname)
@@ -140,6 +265,10 @@ class TestAlertsTrader(unittest.TestCase):
         mock_brokerage = create_autospec(TDA)
         self.tracker_order(mock_brokerage)
 
+    def test_tracker_sto_TDA(self):
+        mock_brokerage = create_autospec(TDA)
+        self.tracker_order_sto(mock_brokerage)
+        
     def test_tracker_etrade(self):
         mock_brokerage = create_autospec(eTrade)
         self.tracker_order(mock_brokerage)
