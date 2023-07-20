c5f511a8733727c83d7e479e935195ea41faa4f4
Adonay Nunes <adonay.s.nunes@gmail.com>
2023-07-20 09:47:37 -0400

    fix live pnl

--- DiscordAlertsTrader/gui_generator.py
+++ DiscordAlertsTrader/gui_generator.py
@@ -390,20 +390,21 @@ def compute_live_trader_port(trade, order):
     elif trade["Type"] == "STO":
         stc_PnL = float((bto_price - stc_price)/bto_price) *100
 
-    sold_tot = np.nansum([trade[f"STC{i}-Qty"] for i in range(1,4)])
     # get STC number not yet filled
     for i in range(1,4):
         STC = f"STC{i}"
         if pd.isnull(trade[f"STC{i}-Qty"]):
             break
 
+    sold_tot = np.nansum([trade[f"STC{i}-Qty"] for i in range(1,4)])
     #Log portfolio
     trade[ STC + "-Price"] = stc_price
     trade[ STC + "-Price-alert"] = stc_price
     trade[ STC + "-Price-actual"] = stc_price
     trade[ STC + "-PnL"] = stc_PnL
     trade[ STC + "-Qty"] = trade['filledQty'] - sold_tot
-
+    
+    sold_tot = np.nansum([trade[f"STC{i}-Qty"] for i in range(1,4)])
     stc_PnL_all = np.nansum([trade[f"STC{i}-PnL"]*trade[f"STC{i}-Qty"] for i in range(1,4)])/sold_tot
     trade[ "PnL"] = stc_PnL_all
 
